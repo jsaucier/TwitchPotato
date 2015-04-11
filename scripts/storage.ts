17cac522d04bf67d66c3edb6477cacfbb9b29a2c
jsaucier <joseph.saucier@gmail.com>
2015-04-11 08:25:44 -0500

    - Code cleanup.

--- scripts/storage.ts
+++ scripts/storage.ts
@@ -2,28 +2,64 @@ module TwitchPotato {
     "use strict";
 
     export class StorageHandler {
-        /* An array list of our settings. */
-        public settings: StorageData;
+        /** The storage settings. */
+        private settings: StorageData;
 
-        /* Loads the settings. */
-        public Load(callback?: Callback): void {
+        /** Gets the stored users. */
+        GetUsers(): string[] {
+            return this.settings.users;
+        }
+
+        /** Adds the users to the storage. */
+        AddUser(user: string): boolean {
+            /** Ensure the user isn't already added. */
+            if (this.settings.users.indexOf(user) !== -1) return false;
+
+            this.settings.users.push(user);
+
+            this.Save();
+
+            return true;
+        }
+
+        /** Removes the user from the storage. */
+        RemoveUser(user: string): void {
+            this.settings.users.splice(
+                this.settings.users.indexOf(user), 1);
+
+            this.Save();
+        }
+
+        /** Set the zoom level. */
+        SetZoom(zoom: number): void {
+            this.settings.zoom = zoom;
+
+            this.Save();
+        }
+
+        /** Get the zoom level. */
+        GetZoom(): number {
+            return this.settings.zoom;
+        }
+
+        /** Loads the settings. */
+        Load(callback?: EmptyCallback): void {
             chrome.storage.local.get(null, (store) => {
-                /* Set the default value. */
+                /** Set the default value. */
                 if ($.isEmptyObject(store) === true)
                     this.LoadDefaults(callback);
                 else
-                    /* Set the settings. */
+                    /** Set the settings. */
                     this.settings = store.settings;
 
-                /* Call the callback. */
-                Application.OnStorageLoaded();
+                /** Fire the callback. */
                 if (typeof (callback) === 'function')
                     callback();
             });
         }
 
-        /* Loads the default settings. */
-        public LoadDefaults(callback?: Callback): void {
+        /** Loads the default settings. */
+        LoadDefaults(callback?: EmptyCallback): void {
             this.settings = {
                 zoom: 100,
                 users: []
@@ -32,21 +68,21 @@ module TwitchPotato {
             this.ClearStorage(callback);
         }
 
-        /* Clears the storage. */
-        private ClearStorage(callback?: Callback): void {
+        /** Savaes the settings. */
+        Save(callback?: EmptyCallback): void {
+            chrome.storage.local.set({
+                settings: this.settings
+            }, callback);
+        }
+
+        /** Clears the storage. */
+        private ClearStorage(callback?: EmptyCallback): void {
             chrome.storage.local.clear(() => {
                 chrome.storage.sync.clear(() => {
                     Application.ShowError('The settings have been reset to defaults.')
                     this.Save(callback);
                 });
             });
-        }
-
-        /* Savaes the settings. */
-        public Save(callback?: Callback): void {
-            chrome.storage.local.set({
-                settings: this.settings
-            }, callback);
         }
     }
 }
