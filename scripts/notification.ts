732bc69482d98a98c06b4fda8fb4cd2bf83bd386
jsaucier <joseph.saucier@gmail.com>
2015-04-11 08:19:42 -0500

    - Code cleanup.

--- scripts/notification.ts
+++ scripts/notification.ts
@@ -1,30 +1,28 @@
 module TwitchPotato {
 
     export class NotificationHandler {
-
         /** Dictionary containing the online channels and the current game for the channel. */
         private online: Dictionary<string> = {};
-
-        constructor() { }
 
         /**
          * Displays a notification of the new channels that are online or when
          * a channel changes games. */
-        public Notify() {
-
+        Notify() {
             $('#notification ul').empty();
 
             /** The online followed channels. */
             var followed = Application.Twitch.GetFollows(FollowType.Channel);
 
-            /** Temporary dictionary containing the online channels and the current game for the channel. */
+            /**
+             * Temporary dictionary containing the online channels and
+             * the current game for the channel. */
             var online: Dictionary<string> = {};
 
             /** The data for the channel. */
             var channel: Channel;
 
             for (var o in followed) {
-                /* Update the channel. */
+                /** Update the channel. */
                 channel = Application.Twitch.GetMenu(MenuType.Channels)[o];
 
                 /**
@@ -36,37 +34,34 @@ module TwitchPotato {
                 }
             }
 
-            /* Add the online channels to the notification window. */
+            /** Add the online channels to the notification window. */
             for (var o in online) {
-                /* Update the channel. */
+                /** Update the channel. */
                 channel = Application.Twitch.GetMenu(MenuType.Channels)[o];
 
                 /** The notification item template. */
-                var item = $($('#notify-template').html());
+                var html = $('#notify-item-template').html().format(
+                    channel.streamer, channel.game);
 
-                /* Set the notification data for this channel. */
-                item.find('.streamer').text(channel.streamer);
-                item.find('.game').text(channel.game);
+                /** Append the notification item to the container. */
+                $('#notification ul').append(html);
 
-                /* Append the notification item to the container. */
-                item.appendTo($('#notification ul'));
-
-                /* Track the user as now online. */
+                /** Track the user as now online. */
                 this.online[o] = channel.game || '';
             }
 
             /** Temporary dictionary containing only online channels.  */
             var cleanup: Dictionary<string> = {};
 
-            /* Iterate the online channels and ensure they are truely online. */
+            /** Iterate the online channels and ensure they are truely online. */
             for (var i in this.online)
-                /* Ensure channel is online. */
+                /** Ensure channel is online. */
                 if (followed[i] !== undefined) cleanup[i] = this.online[i];
 
-            /* Update the online channels. */
+            /** Update the online channels. */
             this.online = cleanup;
 
-            /* Display the notification window. */
+            /** Display the notification window. */
             if ($('#notification li').length > 0) {
                 $('#notification').fadeIn(function() {
                     setTimeout(function() {
