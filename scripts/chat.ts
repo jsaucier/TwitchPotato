253c90bb6d6167c89685694b965af50ba2cbb9d1
jsaucier <joseph.saucier@gmail.com>
2015-04-11 08:17:08 -0500

    Code cleanup.

--- scripts/chat.ts
+++ scripts/chat.ts
@@ -22,28 +22,23 @@ module TwitchPotato {
 
         constructor() { }
 
-        /**
-         * Gets whether the chat is shown.
-         */
+        /** Gets whether the chat is shown. */
         IsShown(): boolean {
             return this.isShown;
         }
 
-        /**
-         * Closes the chat window.
-         */
+        /** Closes the chat window. */
         Close() {
             /** Fades the chat window out. */
             this.chat.fadeOut();
+
             /** Set the chat as no longer shown. */
             this.isShown = false;
         }
 
-        /**
-         * Shows the chat window for the specified channel.
-         */
+        /** Shows the chat window for the specified channel. */
         Show(channel: string): void {
-            /* Toggle visibility. */
+            /** Toggle visibility. */
             if (this.isShown === true) {
                 /** Update the player layout so that it is not in dock layout.  */
                 Application.Player.UpdateLayout(false, PlayersLayout.Full);
@@ -51,35 +46,33 @@ module TwitchPotato {
                 return this.Close();
             }
 
-            /* Ensure the channel is not already loaded. */
+            /** Ensure the channel is not already loaded. */
             if (channel !== undefined &&
                 channel !== this.chat.attr('channel')) {
-                /* Set the webview source. */
+                /** Set the webview source. */
                 $(this.webview).attr('src', this.chatUrl.format(channel));
 
-                /* Catch the webview load events. */
+                /** Catch the webview load events. */
                 this.webview.addEventListener('loadcommit', () => {
-                    /* The webview is now loaded. */
+                    /** The webview is now loaded. */
                     this.isLoaded = true;
 
-                    /* Inject the chat css. */
+                    /** Inject the chat css. */
                     this.webview.insertCSS({ file: 'css/twitch.css' });
 
-                    /* Update the zoom. */
+                    /** Update the zoom. */
                     this.UpdateZoom();
                 });
 
-                /* Set the channel attribute. */
+                /** Set the channel attribute. */
                 this.chat.attr('channel', channel);
             }
 
-            /* Update the chat layout. */
+            /** Update the chat layout. */
             this.UpdateLayout();
         }
 
-        /**
-         * Toggles the chat visibility on guide toggle.
-         */
+        /** Toggles the chat visibility on guide toggle. */
         ToggleChat(show, isGuideToggle): void {
             if (show === true &&
                 this.isShown === true)
@@ -88,40 +81,36 @@ module TwitchPotato {
                 this.chat.hide();
         }
 
-        /**
-         * Updates the font-size of the chat based on the zoom level.
-         */
+        /** Updates the font-size of the chat based on the zoom level. */
         UpdateZoom(): void {
-            /* Cannot update the font-size if the webview is not loaded. */
+            /** Cannot update the font-size if the webview is not loaded. */
             if (this.isLoaded === false) return;
 
-            /* Set the zoom. */
+            /** Set the zoom. */
             this.webview.insertCSS({
-                code: 'body { font-size: {0}%!important }'.format(Application.Storage.settings.zoom)
+                code: 'body { font-size: {0}%!important }'.format(Application.Storage.GetZoom())
             });
         }
 
-        /**
-         * Updates the layout for the chat window.
-         */
+        /** Updates the layout for the chat window. */
         UpdateLayout(direction = Direction.Down, isGuideToggle = false): void {
-            /* Ensure the chat is actually shown. */
+            /** Ensure the chat is actually shown. */
             if (this.isShown !== true) return;
 
-            /* Determine the new layout. */
+            /** Determine the new layout. */
             if (direction === Direction.Left) this.layout--;
             else if (direction === Direction.Right) this.layout++;
 
             /** The size of the layouts enum. */
             var size = Object.keys(ChatLayout).length / 2;
 
-            /* Bounds for the enum. */
+            /** Bounds for the enum. */
             if (this.layout < 0)
                 this.layout = size - 1;
             else if (this.layout > size - 1)
                 this.layout = 0;
 
-            /* Update the chat layout. */
+            /** Update the chat layout. */
             this.chat
                 .hide()
                 .attr('layout', this.layout)
