fac62dd786abb5ef0633d4e8a6f2c60b2bd4d326
Joseph Saucier <joseph.saucier@gmail.com>
2015-05-12 14:53:46 -0500

    Continued work.

--- Source/Scripts/App/js/Player/menu.ts
+++ Source/Scripts/App/js/Player/menu.ts
@@ -3,6 +3,7 @@ module TwitchPotato {
     export class PlayerMenu {
 
         private _player: Player;
+        private _isShown = false;
 
         private _sources: Array<string>
 
@@ -23,6 +24,36 @@ module TwitchPotato {
             this.CreateNotification;
             this.CreateMenu();
         }
+
+        HandleInput(input: Inputs): boolean {
+
+            if (!this._isShown) return false;
+
+            switch (input) {
+
+                case Inputs.Left:
+                    this.UpdateSelection(Direction.Left);
+                    return true;
+
+                case Inputs.Right:
+                    this.UpdateSelection(Direction.Right);
+                    return true;
+
+                case Inputs.Select:
+                    this.SelectItem();
+                    return true;
+
+                case Inputs.ContextMenu:
+                    this.ShowMenu(undefined, false, true)
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        /** Gets if the menu is shown. */
+        IsShown(): boolean { return this._isShown; }
 
         /** Highlights the selected player. */
         Highlight(showOrHide: boolean): void {
@@ -64,39 +95,96 @@ module TwitchPotato {
             this._notification = this._player.Container().find('.notification');
         }
 
+        /** Shows the player menu. */
+        ShowMenu(action?: PlayerActions, show = true, fade = true): void {
 
-        /** Creates the Qaulity menu items. */
-        CreateQualityMenu(): void {
+            this._menu.find('.items').empty();
+
+            if (action !== undefined) {
+
+                switch (action) {
+
+                    case -1:
+                        this.ShowMainMenu();
+                        break;
+
+                    case PlayerActions.Quality:
+                        this.ShowQualityMenu();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            if (!fade)
+                this._menu.toggle(show);
+            else {
+                if (show)
+                    this._menu.fadeIn();
+                else
+                    this._menu.fadeOut();
+            }
+
+            this._isShown = show;
+
+            this.ResetTimeout();
+        }
+
+        /** Creates the Main menu item.s */
+        private ShowMainMenu(): void {
+
+            this.AddMenuItem(PlayerActions.Position, -1, -1, 'fullscreen');
+            this.AddMenuItem(PlayerActions.Mute, -1, 0, 'fullscreen');
+            this.AddMenuItem(PlayerActions.Quality, -1, 0, 'fullscreen');
+            this.AddMenuItem(PlayerActions.ViewMode, -1, 0, 'fullscreen');
+            this.AddMenuItem(PlayerActions.Chat, -1, 0, 'fullscreen');
+            this.AddMenuItem(PlayerActions.Layout, -1, 0, 'fullscreen');
+            this.AddMenuItem(-1, -1, 0, 'fullscreen');
+        }
+
+        /** Creates the Quality menu items. */
+        private ShowQualityMenu(): void {
 
             for (var i = 0; i < Object.keys(Quality).length / 2; i++) {
-
-                var item = $('<div/>')
-                    .addClass('item')
-                    .attr({
-                        action: PlayerActions.Quality,
-                        quality: i
-                    });
-
-                item.append($('<img/>').
-                    attr('src', 'images/quality-{0}.png'.format(Quality[i].toLowerCase())));
-
-                // item.append($('<div/>')
-                //     .addClass('text')
-                //     .text(Quality[i]));
-
-                if (this._player.Quality() === i)
-                    item.addClass('selected');
-
-                this._menu.find('.items').append(item);
+                this.AddMenuItem(
+                    PlayerActions.Quality,
+                    i,
+                    this._player.Quality(),
+                    Quality[i]);
             }
         }
 
         /** Creates the Position menu items. */
-        CreatePositionMenu(): void {
+        private ShowPositionMenu(): void {
 
-            var size = Object.keys(MultiPosition).length / 2;
+            var positions: Array<MultiPosition>;
 
-            var html = $('<')
+            if (this._player.MultiLayout() === MultiLayout.Equal) {
+                positions.push(MultiPosition.TopLeft);
+                positions.push(MultiPosition.TopRight);
+                positions.push(MultiPosition.BottomLeft);
+                positions.push(MultiPosition.BottomRight);
+            }
+            else if (this._player.MultiLayout() === MultiLayout.Default) {
+                positions.push(MultiPosition.TopLeft);
+                positions.push(MultiPosition.Top);
+                positions.push(MultiPosition.TopRight);
+                positions.push(MultiPosition.Left);
+                positions.push(MultiPosition.Middle);
+                positions.push(MultiPosition.Right);
+                positions.push(MultiPosition.BottomLeft);
+                positions.push(MultiPosition.Bottom);
+                positions.push(MultiPosition.BottomRight);
+            }
+
+            for (var i = 0; i < positions.length; i++) {
+                this.AddMenuItem(
+                    PlayerActions.Position,
+                    i,
+                    this._player.Position(),
+                    MultiPosition[i]);
+            }
         }
 
         /** Creates the menu element. */
@@ -112,187 +200,68 @@ module TwitchPotato {
             this._menu = this._player.Container().find('.menu');
         }
 
-        private ShowMenu(action?: PlayerActions, show = true, fade = true): void {
+        /** Adds an item to the menu. */
+        private AddMenuItem(action: PlayerActions, value: number, selected: number, image: string): void {
+
+            var item = $('<div/>')
+                .addClass('item')
+                .attr({
+                    action: action,
+                    value: value
+                });
+
+            item.append($('<img/>').
+                attr('src', 'images/{0}.png'
+                    .format(image.toLowerCase())));
+
+            if (selected === value) item.addClass('selected');
+
+            this._menu.find('.items').append(item);
+        }
+
+        /** Updates the selected menu item. */
+        private UpdateSelection(direction: Direction): void {
+
+            var index = this._menu
+                .find('.item')
+                .index(this._menu
+                    .find('.item.selected'));
+
+            if (index === - 1) index = 0;
+
+            if (direction === Direction.Left) index--;
+            else if (direction === Direction.Right) index++;
+
+            var numItems = this._menu.find('.item').length;
+
+            if (index < 0)
+                index = numItems - 1;
+            else if (index > numItems - 1)
+                index = 0;
+
+            this._menu
+                .find('.item.selected')
+                .removeClass('selected');
+
+            this._menu
+                .find('.item')
+                .eq(index)
+                .addClass('selected');
+
+            this.ResetTimeout();
+        }
+
+        /** Selects the menu item. */
+        private SelectItem(): void {
+            console.log(this._menu.find('.item.selected')[0]);
+        }
+
+        /** Resets the menu timeout. */
+        private ResetTimeout(): void {
 
             clearTimeout(this._menuTimeout);
 
-            this._menu.find('.items').empty();
-
-            if (action !== undefined) {
-
-                switch (action) {
-
-                    case PlayerActions.Quality:
-                        this.CreateQualityMenu();
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            if (show) {
-                if (fade)
-                    this._menu.fadeIn();
-                else
-                    this._menu.show();
-            }
-            else {
-                if (fade)
-                    this._menu.fadeOut();
-                else
-                    this._menu.hide();
-            }
-
-            // this._menuTimeout = setTimeout(() => this.ShowMenu(undefined, false, true), 5000);
+            this._menuTimeout = setTimeout(() => this.ShowMenu(undefined, false, true), 5000);
         }
     }
 }
-//
-//     export enum PipMode {
-//         Default,
-//         Equal
-//     }
-//
-//     export enum PipPosition {
-//         Full,
-//         Top,
-//         Left,
-//         Right,
-//         Bottom,
-//         Middle,
-//         TopLeft,
-//         TopRight,
-//         BottomLeft,
-//         BottomRight
-//     }
-//
-//     export enum ViewMode {
-//         Fullscreen,
-//         Windowed
-//     }
-//
-//     interface OptionButtonData {
-//         id: string;
-//         text: string;
-//     }
-//
-//     /** Instance of the PlayerOptionsHandler */
-//     export class PlayerOptionsHandler {
-//
-//         /** The picture in picture mode. */
-//         private _pipMode = PipMode.Default;
-//
-//         /** The default picture in picture mode position. */
-//         private _defaultPipModePosition = DefaultPipModePosition.Full;
-//
-//         /** The equal picture in picture mode position. */
-//         private _equalPipModePosition = EqualPipModePosition.TopLeft;
-//
-//         /** The current view mode. */
-//         private _viewMode = ViewMode.Fullscreen;
-//
-//         /** The current quality mode. */
-//         private _quality: Quality;
-//
-//         /** The current mute status. */
-//         private _isMuted = false;
-//
-//         /** The current chat status. */
-//         private _isChatShown = false;
-//
-//         /** Processes all input for the player options menu. */
-//         HandleInput(input: Inputs): boolean {
-//             return false;
-//         }
-//
-//         /** Gets the player options JQuery. */
-//         private GetOptionsElement(): JQuery {
-//             return $('#selector #options');
-//         }
-//
-//         /** Creates the menu buttons. */
-//         private CreateMenu(buttons: Array<OptionButtonData>): void {
-//
-//             var opts = this.GetOptionsElement();
-//         }
-//
-//         /** Updates the menu. */
-//         private UpdateMenu(direction: Direction): void { }
-//
-//         /** Gets the image for the button. */
-//         private GetImage(buttonId: string): string {
-//
-//             switch (buttonId) {
-//                 case 'cancel':
-//                     return 'cancel.png';
-//                 case 'position':
-//                     if (this._pipMode === PipMode.Default)
-//                         return DefaultPipModePosition[this._defaultPipModePosition] + '.png';
-//                     else if (this._pipMode === PipMode.Equal)
-//                         return EqualPipModePosition[this._equalPipModePosition] + '.png';
-//                 case 'mute':
-//                     return (this._isMuted) ? 'mute.png' : 'unmute.png';
-//                 case 'quality':
-//                     return Quality[this._quality].toLowerCase() + '.png';
-//                 case 'viewMode':
-//                     return ViewMode[this._viewMode].toLowerCase() + '.png';
-//             }
-//
-//             return '';
-//         }
-//
-//         /** Shows the main options menu. */
-//         private ShowMenu(): void {
-//
-//             var buttons: Array<OptionButtonData> = [
-//                 { id: 'position', text: 'Position' },
-//                 { id: 'mute', text: 'Toggle Mute' },
-//                 { id: 'quality', text: 'Change Quality' },
-//                 { id: 'viewMode', text: 'Change View Mode' },
-//                 { id: 'cancel', text: 'Cancel' },
-//             ];
-//
-//             this.CreateMenu(buttons);
-//         }
-//
-//         /** Shows the position options menu. */
-//         private ShowPositionMenu(): void {
-//
-//             var buttons: Array<OptionButtonData>;
-//
-//             if (this._pipMode === PipMode.Default) {
-//                 buttons = [
-//                     { id: 'top', text: 'Top' },
-//                     { id: 'left', text: 'Left' },
-//                     { id: 'right', text: 'Right' },
-//                     { id: 'bottom', text: 'Bottom' },
-//                     { id: 'middle', text: 'Middle' },
-//                     { id: 'topLeft', text: 'Top Left' },
-//                     { id: 'topRight', text: 'Top Right' },
-//                     { id: 'bottomLeft', text: 'Bottom Left' },
-//                     { id: 'bottomRight', text: 'Bottom Right' },
-//                 ];
-//             }
-//             else if (this._pipMode === PipMode.Equal) {
-//                 buttons = [
-//                     { id: 'topLeft', text: 'Top Left' },
-//                     { id: 'topRight', text: 'Top Right' },
-//                     { id: 'bottomLeft', text: 'Bottom Left' },
-//                     { id: 'bottomRight', text: 'Bottom Right' },
-//                 ];
-//             }
-//
-//             this.CreateMenu(buttons);
-//         }
-//
-//         /** Shows the mute options menu. */
-//         private ShowMuteOptions(): void { }
-//
-//         /** Shows the quality options menu. */
-//         private ShowQualityOptions(): void { }
-//
-//         /** Shoes the view mode options menu. */
-//         private ShowViewModeOptions(): void { }
-//
-//     }
-// }
